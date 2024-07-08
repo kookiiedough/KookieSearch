dd28aa1a4aa13e48f3adf8bb5f056a20f9885ba7
Sharleen Chebet <kookiiiedough@gmail.com>
2024-07-08 01:13:03 +0000

    del

--- query.js
+++ query.js
@@ -3,7 +3,7 @@ function submit() {
   document.getElementById('content').innerHTML = '';
   var val = document.getElementById('search').value;
   var newelement = document.createElement('script');
-  newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAM0oOOQS0bFw5S5wl5Ko1MrmCQ2BJVXBc&cx=036b3b9a18a5443fd&q=${val}&callback=hndlr`;
+  newelement.src = `https://www.googleapis.com/customsearch/v1?key=API_KEY=${val}&callback=hndlr`;
   newelement.id = 'mainscript';
   document.body.appendChild(newelement);
 }
