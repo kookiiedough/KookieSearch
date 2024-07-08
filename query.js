812914fceef84c0384fec987c2fff0caffdc3cca
Sharleen Chebet <kookiiiedough@gmail.com>
2024-07-08 00:35:55 +0000

    done

--- query.js
+++ query.js
@@ -1,11 +1,9 @@
 function submit() {
-    document.getElementById('buttons').style.display = 'block';
-    document.getElementById('content').innerHTML = '';
-    var val = document.getElementById('search').value;
-    var newelement = document.createElement('script');
-    newelement.src = `https://www.googleapis.com/customsearch/v1?key=API_KEY&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
-    newelement.id = 'mainscript';
-    document.body.appendChild(newelement);
-  }
-  
-  // add your own API key to replace 'API_KEY'
+  document.getElementById('buttons').style.display = 'block';
+  document.getElementById('content').innerHTML = '';
+  var val = document.getElementById('search').value;
+  var newelement = document.createElement('script');
+  newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAM0oOOQS0bFw5S5wl5Ko1MrmCQ2BJVXBc&cx=036b3b9a18a5443fd&q=${val}&callback=hndlr`;
+  newelement.id = 'mainscript';
+  document.body.appendChild(newelement);
+}
